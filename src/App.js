876cb4b9564717b89f0d6648bdc5a7f8d845b5ac
rayyo <reiyoshizawa0418@gmail.com>
2018-12-11 18:20:40 -0800

    Change component structure using react router and pass data to Event component

--- src/App.js
+++ src/App.js
@@ -5,16 +5,102 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Event from "./components/Event/Event";
 import Home from "./components/Home/Home";
+import SearchKeyword from "./components/SearchKeyword/SearchKeyword";
+
+const PERSONAL_OAUTH_TOKEN = "ZVYAWGFJWR3NSCZX6R4F";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventsDatas: [],
+      logo: "",
+      value: "",
+      date: "",
+      category: "",
+      searchKeyword: ""
+    };
+    this.getApi();
+  }
+
+  getApi = async () => {
+    const response = await fetch(
+      `http://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search/?token=${PERSONAL_OAUTH_TOKEN}`
+    );
+    if (response.status === 200) {
+      const json = await response.json();
+      const events = await json.events;
+      console.log(events);
+      this.setState({
+        eventsDatas: events
+      });
+    }
+  };
+  searchApi = async () => {
+    const { value, date, category } = this.state;
+    const response = await fetch(
+      `http://cors-anywhere.herokuapp.com/https://www.eventbriteapi.com/v3/events/search/?location.address=${value}&start_date.keyword=${date}&categories=${category}&token=${PERSONAL_OAUTH_TOKEN}`
+    );
+    if (response.status === 200) {
+      const json = await response.json();
+      const events = await json.events;
+      console.log(events);
+      this.setState({
+        eventsDatas: events,
+        searchKeyword: <SearchKeyword value={value} />
+      });
+    } else {
+      this.setState({
+        searchKeyword: ""
+      });
+    }
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.searchApi();
+  };
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  hadnleDate = e => {
+    this.setState({
+      date: e.target.value
+    });
+  };
+  hadnleCategory = e => {
+    this.setState({
+      category: e.target.value
+    });
+  };
+
   render() {
+    const { eventsDatas, value, date } = this.state;
     return (
       <Router>
         <div className="App">
           <Header />
-          <Route exact path="/" component={Home} />
-          <Route path="/about" render={() => <div>about</div>} />
-          <Route path="/event/:id" component={Event} />
+          <Route
+            exact
+            path="/"
+            render={props => (
+              <Home
+                {...props}
+                value={value}
+                date={date}
+                eventsDatas={eventsDatas}
+                handleSubmit={this.handleSubmit}
+                handleChange={this.handleChange}
+                hadnleDate={this.hadnleDate}
+                hadnleCategory={this.hadnleCategory}
+              />
+            )}
+          />
+          <Route
+            path="/event/:id"
+            render={props => <Event {...props} eventsDatas={eventsDatas} />}
+          />
           <Footer />
         </div>
       </Router>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,71 +1,69 @@
-import React, { Component } from "react";
+import React from "react";
 import "./scss/style.scss";
 
-class SearchForm extends Component {
-  render() {
-    return (
-      <section className="searchForm">
-        <div className="container">
-          <form onSubmit={this.props.handleSubmit}>
-            <div className="inputBlock">
-              <input
-                className="inputKey"
-                onChange={this.props.handleChange}
-                type="text"
-                value={this.props.value}
-                placeholder="Search the place"
-              />
-              <button className="searchBtn" type="submit">
-                <i className="fas fa-search-location fa-lg	" />
-              </button>
-            </div>
-            <div className="selectBlock">
-              <select
-                className="selectBtn"
-                onChange={this.props.hadnleDate}
-                value={this.props.date}
-              >
-                <option value="today">Today</option>
-                <option value="tomorrow">Tomorrow</option>
-                <option value="this_weekend">This weekend</option>
-                <option value="this_week">This week</option>
-                <option value="next_week">Next week</option>
-                <option value="this_month">This month</option>
-                <option value="next_month">Next month</option>
-              </select>
-              <select
-                className="selectBtn"
-                onChange={this.props.hadnleCategory}
-                value={this.props.category}
-              >
-                <option value="103">Music</option>
-                <option value="101">Business & Professional</option>
-                <option value="110">Food & Drink</option>
-                <option value="113">Community & Culture</option>
-                <option value="105">Performing & Visual Arts</option>
-                <option value="104">Film, Media & Entertainment</option>
-                <option value="108">Sports & Fitness</option>
-                <option value="107">Health & Wellness</option>
-                <option value="102">Science & Technology</option>
-                <option value="109">Travel & Outdoor</option>
-                <option value="111">Charity & Causes</option>
-                <option value="114">Religion & Spirituality</option>
-                <option value="115">Family & Education</option>
-                <option value="116">Seasonal & Holiday</option>
-                <option value="112">Government & Politics</option>
-                <option value="106">Fashion & Beauty</option>
-                <option value="117">Home & Lifestyle</option>
-                <option value="118">Auto, Boat & Air</option>
-                <option value="119">Hobbies & Special Interest</option>
-                <option value="199">Other</option>
-                <option value="120">School Activities</option>
-              </select>
-            </div>
-          </form>
-        </div>
-      </section>
-    );
-  }
-}
+const SearchForm = props => {
+  return (
+    <section className="searchForm">
+      <div className="container">
+        <form onSubmit={props.handleSubmit}>
+          <div className="inputBlock">
+            <input
+              className="inputKey"
+              onChange={props.handleChange}
+              type="text"
+              value={props.value}
+              placeholder="Search the place"
+            />
+            <button className="searchBtn" type="submit">
+              <i className="fas fa-search-location fa-lg	" />
+            </button>
+          </div>
+          <div className="selectBlock">
+            <select
+              className="selectBtn"
+              onChange={props.hadnleDate}
+              value={props.date}
+            >
+              <option value="today">Today</option>
+              <option value="tomorrow">Tomorrow</option>
+              <option value="this_weekend">This weekend</option>
+              <option value="this_week">This week</option>
+              <option value="next_week">Next week</option>
+              <option value="this_month">This month</option>
+              <option value="next_month">Next month</option>
+            </select>
+            <select
+              className="selectBtn"
+              onChange={props.hadnleCategory}
+              value={props.category}
+            >
+              <option value="103">Music</option>
+              <option value="101">Business & Professional</option>
+              <option value="110">Food & Drink</option>
+              <option value="113">Community & Culture</option>
+              <option value="105">Performing & Visual Arts</option>
+              <option value="104">Film, Media & Entertainment</option>
+              <option value="108">Sports & Fitness</option>
+              <option value="107">Health & Wellness</option>
+              <option value="102">Science & Technology</option>
+              <option value="109">Travel & Outdoor</option>
+              <option value="111">Charity & Causes</option>
+              <option value="114">Religion & Spirituality</option>
+              <option value="115">Family & Education</option>
+              <option value="116">Seasonal & Holiday</option>
+              <option value="112">Government & Politics</option>
+              <option value="106">Fashion & Beauty</option>
+              <option value="117">Home & Lifestyle</option>
+              <option value="118">Auto, Boat & Air</option>
+              <option value="119">Hobbies & Special Interest</option>
+              <option value="199">Other</option>
+              <option value="120">School Activities</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
 
 export default SearchForm;
