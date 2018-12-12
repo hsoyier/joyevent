1755069014e1e071e7e65e7a2f01a543f3647d0f
rayyo <reiyoshizawa0418@gmail.com>
2018-12-11 18:33:00 -0800

    Change component structure using react router and pass data to Event component

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -1,13 +1,32 @@
 import React from "react";
+import "./scss/style.scss";
 
-class Event extends React.Component {
-  render() {
-    return (
-      <div>
-        <p>dsdfsdffssf</p>
+const Event = props => {
+  const id = props.match.params.id;
+  const event = props.eventsDatas.find(event => event.id === id);
+  console.log(event);
+  return (
+    <div className="eventItem">
+      <div className="eventItem__inner">
+        <div className="eventItem__upper row">
+          {event ? (
+            <div
+              className="eventItem__image col-md-8"
+              style={{
+                backgroundImage: `url(${event.logo.original.url})`
+              }}
+            />
+          ) : null}
+          <div className="eventItem__info col-md-4">
+            <p className="font-weight-bold">{event && event.name.text}</p>
+          </div>
+        </div>
+        <div className="eventItem__buttom">
+          <p>{event && event.description.text}</p>
+        </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Event;
--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,47 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Event from "../Event/Event";
+import { Link } from "react-router-dom";
 import "./scss/style.scss";
 
-class Events extends React.Component {
-  render() {
-    return (
-      <section className="event">
-        <div className="container">
-          <ul className="row event__list">
-            {this.props.eventsDatas.map(event => {
-              return (
-                <Link
-                  to={`/event/${event.id}`}
-                  className="col-md-4 event__item"
-                  key={event.id}
-                >
-                  <li key={event.id}>
-                    {event.logo ? (
-                      <div
-                        className="img__box"
-                        style={{
-                          backgroundImage: `url(
+const Events = props => {
+  return (
+    <section className="event">
+      <div className="container">
+        <ul className="row event__list">
+          {props.eventsDatas.map(event => {
+            return (
+              <Link
+                to={`/event/${event.id}`}
+                className="col-md-4 event__item"
+                key={event.id}
+              >
+                <li key={event.id}>
+                  {event.logo ? (
+                    <div
+                      className="img__box"
+                      style={{
+                        backgroundImage: `url(
                   ${event.logo.url}
                   )`
-                        }}
-                      />
-                    ) : (
-                      <div className="dummy" />
-                    )}
-                    <div className="event__infos">
-                      <h3 className="event__title">{event.name.text}</h3>
-                      <p className="event__time">{event.start.local}</p>
-                    </div>
-                  </li>
-                </Link>
-              );
-            })}
-          </ul>
-        </div>
-      </section>
-    );
-  }
-}
+                      }}
+                    />
+                  ) : (
+                    <div className="dummy" />
+                  )}
+                  <div className="event__infos">
+                    <h3 className="event__title">{event.name.text}</h3>
+                    <p className="event__time">{event.start.local}</p>
+                  </div>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
 
 export default Events;
